f1b3a2739a9510e4509a5a252acb35942597d9a4
hcaja <academiclearning22@gmail.com>
2023-10-26 14:29:16 +0800

    checklist and parent profile update

---
+++ flutter_application_1/lib/screens/parent/check_list.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+
+class CustomCheckboxListTile extends StatefulWidget {
+  const CustomCheckboxListTile({Key? key}) : super(key: key);
+
+  @override
+  State<CustomCheckboxListTile> createState() => _CustomCheckboxListTileState();
+}
+
+class _CustomCheckboxListTileState extends State<CustomCheckboxListTile> {
+  bool checkboxValue1 = true;
+  bool checkboxValue2 = true;
+  bool checkboxValue3 = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25),
+      child: Column(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(
+                color: const Color.fromARGB(255, 0, 0, 0), // Border color
+                width: 1.0, // Border width
+              ),
+              borderRadius: BorderRadius.circular(15.0),
+              boxShadow: [
+                // black shadow
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  spreadRadius: 1,
+                  blurRadius: 1,
+                  offset: const Offset(0, 1), // changes position of shadow
+                ),
+              ],
+            ),
+            child: CheckboxListTile(
+              value: checkboxValue1,
+              onChanged: (bool? value) {
+                setState(() {
+                  checkboxValue1 = value ?? false;
+                });
+              },
+              title: const Text(
+                'Headline',
+                style: TextStyle(
+                    fontFamily: 'Poppin', fontWeight: FontWeight.w600),
+              ),
+              controlAffinity: ListTileControlAffinity
+                  .leading, // Place the checkbox on the leading side
+              activeColor: const Color(
+                  0xFF006A5C), // Change the color of the checked checkbox
+              checkColor: Colors.white, // Change the color of the checkmark
+            ),
+          ),
+          const SizedBox(height: 10),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(
+                color: const Color.fromARGB(255, 0, 0, 0), // Border color
+                width: 1.0, // Border width
+              ),
+              borderRadius: BorderRadius.circular(15.0),
+              boxShadow: [
+                // black shadow
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  spreadRadius: 1,
+                  blurRadius: 1,
+                  offset: const Offset(0, 1), // changes position of shadow
+                ),
+              ],
+            ),
+            child: CheckboxListTile(
+              value: checkboxValue2,
+              onChanged: (bool? value) {
+                setState(() {
+                  checkboxValue2 = value ?? false;
+                });
+              },
+              title: const Text(
+                'Headline',
+                style: TextStyle(
+                    fontFamily: 'Poppin', fontWeight: FontWeight.w600),
+              ),
+              controlAffinity: ListTileControlAffinity
+                  .leading, // Place the checkbox on the leading side
+              activeColor: const Color(
+                  0xFF006A5C), // Change the color of the checked checkbox
+              checkColor: Colors.white, // Change the color of the checkmark
+            ),
+          ),
+          const SizedBox(height: 10),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(
+                color: const Color.fromARGB(255, 0, 0, 0), // Border color
+                width: 1.0, // Border width
+              ),
+              borderRadius: BorderRadius.circular(15.0),
+              boxShadow: [
+                // black shadow
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  spreadRadius: 1,
+                  blurRadius: 1,
+                  offset: const Offset(0, 1), // changes position of shadow
+                ),
+              ],
+            ),
+            child: CheckboxListTile(
+              value: checkboxValue3,
+              onChanged: (bool? value) {
+                setState(() {
+                  checkboxValue3 = value ?? false;
+                });
+              },
+              title: const Text(
+                'Kamatyonon nako',
+                style: TextStyle(
+                    fontFamily: 'Poppin', fontWeight: FontWeight.w600),
+              ),
+              controlAffinity: ListTileControlAffinity
+                  .leading, // Place the checkbox on the leading side
+              activeColor: const Color(
+                  0xFF006A5C), // Change the color of the checked checkbox
+              checkColor: Colors.white, // Change the color of the checkmark
+            ),
+          ),
+          const SizedBox(height: 10),
+        ],
+      ),
+    );
+  }
+}
--- flutter_application_1/lib/screens/parent/parent_profile.dart
+++ flutter_application_1/lib/screens/parent/parent_profile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/screens/auth/connection.dart';
 import 'package:flutter_application_1/screens/auth/login_as.dart';
+import 'package:flutter_application_1/screens/parent/check_list.dart';
 import 'package:flutter_application_1/screens/parent/tab_bar.dart';
 
 class ParentProfile extends StatelessWidget {
@@ -212,169 +213,192 @@ class ParentProfile extends StatelessWidget {
           ),
 
           Positioned(
-            child: ListView(children: <Widget>[
-              const SizedBox(height: 20),
+            child: ListView(
+              children: <Widget>[
+                const SizedBox(height: 20),
 
-              // - Tab bar -
-              const Padding(
-                padding: EdgeInsets.only(left: 20, right: 20, bottom: 20),
-                child: ParentTabBar(),
-              ),
+                // - Tab bar -
+                const Padding(
+                  padding: EdgeInsets.only(left: 20, right: 20, bottom: 20),
+                  child: ParentTabBar(),
+                ),
 
-              // Padding added before the CustomTabBar to avoid overlap
-              const SizedBox(height: 50),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  // Profile picture
-                  Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: const Color(0xFF0A5E2D),
-                        width: 1.0,
-                      ),
-                      shape: BoxShape.circle,
-                    ),
-                    child: const CircleAvatar(
-                      radius: 50,
-                      backgroundImage: AssetImage('asset/images/lara.jpg'),
-                    ),
-                  ),
-
-                  // Spacing between profile picture and parent name
-                  const SizedBox(height: 5),
-
-                  // Parent Name
-                  const Text(
-                    'Princess Lara Mae Danica Fiona Marie Delute',
-                    style: TextStyle(
-                      color: Color(0xFF67AFA5),
-                      fontSize: 15,
-                      fontFamily: 'Poppins',
-                      fontWeight: FontWeight.w900,
-                    ),
-                  ),
-
-                  const Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.phone,
-                            color: Color(
-                                0xFF006A5C), // Change the color of the phone icon
-                            size: 14, // Change the size of the phone icon
-                          ),
-                          SizedBox(
-                            width: 5,
-                          ),
-                          Text(
-                            "09457862611",
-                            style: TextStyle(
-                              color: Color(
-                                  0xFF645B5B), // Change the color of the phone number text
-                              fontSize:
-                                  10, // Change the font size of the phone number text
-                            ),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.email,
-                            color: Color(
-                                0xFF006A5C), // Change the color of the email icon
-                            size: 14, // Change the size of the email icon
-                          ),
-                          SizedBox(
-                            width: 5,
-                          ),
-                          Text(
-                            "princesslara@sample.email",
-                            style: TextStyle(
-                              color: Color(
-                                  0xFF645B5B), // Change the color of the email text
-                              fontSize:
-                                  10, // Change the font size of the email text
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-
-                  // Spacing between parent name and bio
-                  const SizedBox(height: 5),
-                  Column(
-                    children: [
-                      const SizedBox(height: 5),
-                      const Text(
-                        "BIO",
-                        style: TextStyle(
-                          color: Color(0xFF999999),
-                          fontSize: 13,
-                          fontFamily: 'Poppins',
-                          fontWeight: FontWeight.w900,
+                // Padding added before the CustomTabBar to avoid overlap
+                const SizedBox(height: 50),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    // Profile picture
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: const Color(0xFF0A5E2D),
+                          width: 1.0,
                         ),
+                        shape: BoxShape.circle,
                       ),
-                      const SizedBox(height: 5),
-
-                      // Bio Text Field
-                      Padding(
-                        padding: const EdgeInsets.only(left: 40.0, right: 40.0),
-                        child: TextFormField(
-                          decoration: const InputDecoration(
-                            hintText: "Enter your bio here...", // Hint text
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.green),
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Color(0xFF006A5C)),
-                            ),
-                          ),
-                        ),
+                      child: const CircleAvatar(
+                        radius: 50,
+                        backgroundImage: AssetImage('asset/images/lara.jpg'),
                       ),
+                    ),
 
-                      const SizedBox(height: 10),
-                      // Buttons for Update and Edit
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          ElevatedButton(
-                            onPressed: () {
-                              // Handle the "Update" button action
-                            },
-                            child: const Text(
-                              "Edit",
+                    // Spacing between profile picture and parent name
+                    const SizedBox(height: 5),
+
+                    // Parent Name
+                    const Text(
+                      'Princess Lara Mae Danica Fiona Marie Delute',
+                      style: TextStyle(
+                        color: Color(0xFF67AFA5),
+                        fontSize: 15,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+
+                    const Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.phone,
+                              color: Color(
+                                  0xFF006A5C), // Change the color of the phone icon
+                              size: 14, // Change the size of the phone icon
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Text(
+                              "09457862611",
                               style: TextStyle(
-                                color: Color(0xFF006A5C),
+                                color: Color(
+                                    0xFF645B5B), // Change the color of the phone number text
+                                fontSize:
+                                    10, // Change the font size of the phone number text
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.email,
+                              color: Color(
+                                  0xFF006A5C), // Change the color of the email icon
+                              size: 14, // Change the size of the email icon
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Text(
+                              "princesslara@sample.email",
+                              style: TextStyle(
+                                color: Color(
+                                    0xFF645B5B), // Change the color of the email text
+                                fontSize:
+                                    10, // Change the font size of the email text
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+
+                    // Spacing between parent name and bio
+                    const SizedBox(height: 5),
+                    Column(
+                      children: [
+                        const SizedBox(height: 5),
+                        const Text(
+                          "BIO",
+                          style: TextStyle(
+                            color: Color(0xFF999999),
+                            fontSize: 13,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+                        const SizedBox(height: 5),
+
+                        // Bio Text Field
+                        Padding(
+                          padding:
+                              const EdgeInsets.only(left: 40.0, right: 40.0),
+                          child: TextFormField(
+                            decoration: const InputDecoration(
+                              hintText: "Enter your bio here...", // Hint text
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.green),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Color(0xFF006A5C)),
                               ),
                             ),
                           ),
-                          const SizedBox(
-                              width: 10), // Add spacing between the buttons
-                          ElevatedButton(
-                            onPressed: () {
-                              // Handle the "Edit" button action
-                            },
-                            child: const Text(
-                              "Update",
+                        ),
+
+                        const SizedBox(height: 10),
+                        // Buttons for Update and Edit
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            ElevatedButton(
+                              onPressed: () {
+                                // Handle the "Update" button action
+                              },
+                              child: const Text(
+                                "Edit",
+                                style: TextStyle(
+                                  color: Color(0xFF006A5C),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                                width: 10), // Add spacing between the buttons
+                            ElevatedButton(
+                              onPressed: () {
+                                // Handle the "Edit" button action
+                              },
+                              child: const Text(
+                                "Update",
+                                style: TextStyle(
+                                  color: Color(0xFF006A5C),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        // spacing between buttons and checklist
+                        const SizedBox(height: 20),
+                        const Padding(
+                          padding: EdgeInsets.only(left: 30.0),
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              'Checklist',
                               style: TextStyle(
-                                color: Color(0xFF006A5C),
+                                fontFamily: 'Poppins',
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xFF67AFA5),
                               ),
                             ),
                           ),
-                        ],
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ]),
+                        ),
+                        const SizedBox(height: 20),
+                        const CustomCheckboxListTile(),
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            ),
           ),
         ]),
       ),
