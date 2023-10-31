48c831e9d38170476dfceada799a718d3be07d5a
christian-garcia <tian.garcia395@gmail.com>
2023-10-31 17:14:10 +0800

    feat: node js api login

---
+++ flutter_application_1/lib/controller/config.dart
@@ -0,0 +1,5 @@
+const baseUrl = 'https://therapeasebackend-production.up.railway.app';
+const clinicLoginUrl = '/clinic/login';
+const employeeLoginUrl = '/employees/login';
+const getProfilesUrl = '/clinic/get-profiles';
+const tokenValidator = '/clinic/is-verify';
---
+++ flutter_application_1/lib/models/clinic_profiles.dart
@@ -0,0 +1,29 @@
+class Employee {
+  int id;
+  String email;
+  String password;
+  String username;
+  String name;
+  String role;
+  int clinicAccount;
+  String address;
+  String contactNo;
+  String age;
+  String sex;
+  String profilePicture;
+
+  Employee({
+    required this.id,
+    required this.email,
+    required this.password,
+    required this.username,
+    required this.name,
+    required this.role,
+    required this.clinicAccount,
+    required this.address,
+    required this.contactNo,
+    required this.age,
+    required this.sex,
+    required this.profilePicture,
+  });
+}
