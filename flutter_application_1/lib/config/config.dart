a2d7345eaa85a84041fecffb7554efa64ee715d0
christian-garcia <tian.garcia395@gmail.com>
2023-11-01 22:50:47 +0800

    refactor: move config to higher level

--- flutter_application_1/lib/config/config.dart
+++ flutter_application_1/lib/config/config.dart
@@ -1,5 +1,6 @@
-const baseUrl = 'https://therapeasebackend-production.up.railway.app';
+const baseUrl = 'http://192.168.0.106:5500';
 const clinicLoginUrl = '/clinic/login';
 const employeeLoginUrl = '/employees/login';
 const getProfilesUrl = '/clinic/get-profiles';
 const tokenValidator = '/clinic/is-verify';
+const parentLoginUrl = '/parent/login';
