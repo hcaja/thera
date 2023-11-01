d19681baffad4d917e3c0ebee1423c343ceba4f1
christian-garcia <tian.garcia395@gmail.com>
2023-11-02 03:44:09 +0800

    chore: add validation and toaster dependency

--- flutter_application_1/lib/controller/httplogin_controller.dart
+++ flutter_application_1/lib/controller/httplogin_controller.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/models/clinic_profiles.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart' as http;
 import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -8,18 +9,29 @@ import 'package:flutter_application_1/config/config.dart';
 
 class ClinicLogin {
   late SharedPreferences prefs;
-  Future<void> clinicLogin(username, password) async {
+  Future<bool> clinicLogin(username, password) async {
     prefs = await SharedPreferences.getInstance();
     var reqBody = {"email": username, "password": password};
     var response = await http.post(Uri.parse(baseUrl + clinicLoginUrl),
         headers: {"Content-Type": "application/json"},
         body: jsonEncode(reqBody));
     var jsonResponse = jsonDecode(response.body);
-    if (jsonResponse['access'] != null) {
-      var myToken = jsonResponse['access'];
-      prefs.setString('clinicToken', myToken);
+    if (response.statusCode == 200) {
+      if (jsonResponse['access'] != null) {
+        var myToken = jsonResponse['access'];
+        prefs.setString('clinicToken', myToken);
+      }
+      return true;
     } else {
-      // print('login failed');
+      Fluttertoast.showToast(
+          msg: jsonResponse,
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0);
+      return false;
     }
   }
 
@@ -54,43 +66,59 @@ class ClinicLogin {
 
 class EmployeeLogin {
   late SharedPreferences prefs;
-  Future<void> employeeLogin(BuildContext context, username, password) async {
+  Future<bool> employeeLogin(BuildContext context, username, password) async {
     prefs = await SharedPreferences.getInstance();
     String? token = prefs.getString('clinicToken');
     var reqBody = {"email": username, "password": password};
     var response = await http.post(Uri.parse(baseUrl + employeeLoginUrl),
         headers: {"Content-Type": "application/json", "token": token!},
         body: jsonEncode(reqBody));
-
+    var jsonResponse = jsonDecode(response.body);
     if (response.statusCode == 200) {
-      var jsonResponse = jsonDecode(response.body);
-
       if (jsonResponse['access'] != null) {
         var myToken = jsonResponse['access'];
         prefs.setString('employeeToken', myToken);
-      } else {
-        // print(response.statusCode);
       }
+      return true;
     } else {
-      // print(response.statusCode);
+      Fluttertoast.showToast(
+          msg: jsonResponse,
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0);
+      return false;
     }
   }
 }
 
 class ParentLoginController {
   late SharedPreferences prefs;
-  Future<void> parentLogin(BuildContext context, username, password) async {
+  Future<bool> parentLogin(BuildContext context, username, password) async {
     prefs = await SharedPreferences.getInstance();
     var reqBody = {"email": username, "password": password};
     var response = await http.post(Uri.parse(baseUrl + parentLoginUrl),
         headers: {"Content-Type": "application/json"},
         body: jsonEncode(reqBody));
     var jsonResponse = jsonDecode(response.body);
-    if (jsonResponse['access'] != null) {
-      var myToken = jsonResponse['access'];
-      prefs.setString('parentToken', myToken);
+    if (response.statusCode == 200) {
+      if (jsonResponse['access'] != null) {
+        var myToken = jsonResponse['access'];
+        prefs.setString('parentToken', myToken);
+      }
+      return true;
     } else {
-      //  print('login failed');
+      Fluttertoast.showToast(
+          msg: jsonResponse,
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0);
+      return false;
     }
   }
 
@@ -128,7 +156,6 @@ class Validator {
   Future<bool> validateToken(String token) async {
     var response = await http.get(Uri.parse(baseUrl + tokenValidator),
         headers: {"Content-Type": "application/json", "token": token});
-
     if (response.statusCode == 200) {
       return true;
     } else {
