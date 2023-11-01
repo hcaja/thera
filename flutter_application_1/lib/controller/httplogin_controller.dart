085b2f004450c0117da0c51d1bfae218e86a6070
christian-garcia <tian.garcia395@gmail.com>
2023-11-01 22:56:36 +0800

    feat: add parent login

--- flutter_application_1/lib/controller/httplogin_controller.dart
+++ flutter_application_1/lib/controller/httplogin_controller.dart
@@ -2,12 +2,12 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/models/clinic_profiles.dart';
 import 'package:flutter_application_1/screens/auth/login_profiles.dart';
-import 'package:flutter_application_1/screens/clinic/clinic_profile.dart';
 import 'package:flutter_application_1/screens/parent/dashboard.dart';
+import 'package:flutter_application_1/screens/therapist/ther_profile.dart';
 import 'package:http/http.dart' as http;
 import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import './config.dart';
+import 'package:flutter_application_1/config/config.dart';
 
 class ClinicLogin {
   late SharedPreferences prefs;
@@ -80,7 +80,7 @@ class EmployeeLogin {
 
         Navigator.of(context).pushReplacement(
           MaterialPageRoute(
-            builder: (context) => const Dashboard(),
+            builder: (context) => const TherapistProfile(),
           ),
         );
       } else {
@@ -92,14 +92,63 @@ class EmployeeLogin {
   }
 }
 
+class ParentLoginController {
+  late SharedPreferences prefs;
+  Future<void> parentLogin(BuildContext context, username, password) async {
+    prefs = await SharedPreferences.getInstance();
+    var reqBody = {"email": username, "password": password};
+    var response = await http.post(Uri.parse(baseUrl + parentLoginUrl),
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode(reqBody));
+    var jsonResponse = jsonDecode(response.body);
+    if (jsonResponse['access'] != null) {
+      var myToken = jsonResponse['access'];
+      prefs.setString('parentToken', myToken);
+
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => const Dashboard(),
+        ),
+      );
+    } else {
+      print('login failed');
+    }
+  }
+
+  Future<List<Employee>> getProfiles(BuildContext context, int clinicID) async {
+    prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('clinicToken');
+    Map<String, dynamic> payload = JwtDecoder.decode(token!);
+    var response = await http.get(
+        Uri.parse("$baseUrl$getProfilesUrl/${payload['ID']}"),
+        headers: {"Content-Type": "application/json"});
+    final List<dynamic> jsonData = json.decode(response.body);
+    List<Employee> objects = jsonData
+        .map((json) => Employee(
+              id: json["ID"],
+              email: json["EMAIL"],
+              password: json["PASSWORD"],
+              username: json["USERNAME"],
+              name: json["NAME"],
+              role: json["ROLE"],
+              clinicAccount: json["CLINIC_ACCOUNT"],
+              address: json["ADDRESS"],
+              contactNo: json["CONTACT_NO"],
+              age: json["AGE"],
+              sex: json["SEX"],
+              profilePicture: json["PROFILE_PICTURE"],
+            ))
+        .toList();
+
+    return objects;
+  }
+}
+
 class Validator {
   late SharedPreferences prefs;
   Future<bool> validateToken(String token) async {
-    prefs = await SharedPreferences.getInstance();
-    String? token = prefs.getString('clinicToken');
-
     var response = await http.get(Uri.parse(baseUrl + tokenValidator),
-        headers: {"Content-Type": "application/json", "token": token!});
+        headers: {"Content-Type": "application/json", "token": token});
 
     if (response.statusCode == 200) {
       return true;
