e3c9859ce77c633a4ce2bcaed0290da14723ef83
christian-garcia <tian.garcia395@gmail.com>
2023-11-05 22:01:19 +0800

    chore: ios config

--- flutter_application_1/ios/Runner/AppDelegate.swift
+++ flutter_application_1/ios/Runner/AppDelegate.swift
@@ -8,8 +8,8 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyAdyksru7u8keu8II7yQ-HjAGV5OjV742M")
     GeneratedPluginRegistrant.register(with: self)
+    GMSServices.setMetalRendererEnabled(true)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
