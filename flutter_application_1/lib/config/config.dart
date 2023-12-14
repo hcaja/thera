61862b265c4f80b1f342f76ef1c533114a76d649
hcaja <academiclearning22@gmail.com>
2023-12-14 21:15:52 +0800

    changed ipHost to railwayHost

--- flutter_application_1/lib/config/config.dart
+++ flutter_application_1/lib/config/config.dart
@@ -1,6 +1,6 @@
 const railwayHost = 'https://therapeasebackend-production.up.railway.app';
 const ipHost = 'http://192.168.0.30:5500';
-const baseUrl = ipHost;
+const baseUrl = railwayHost;
 const clinicLoginUrl = '/clinic/login';
 const clinicRegisterUrl = '/clinic/register';
 const clinicTherapistRegisterUrl = '/employees/register';
