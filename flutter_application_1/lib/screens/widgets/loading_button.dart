ff769b66af3ec699a20b18c784c3528dc4190546
christian-garcia <tian.garcia395@gmail.com>
2023-12-16 07:43:25 +0800

    fix: loading button

--- flutter_application_1/lib/screens/widgets/loading_button.dart
+++ flutter_application_1/lib/screens/widgets/loading_button.dart
@@ -5,18 +5,24 @@ class CircularLoadingButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          backgroundColor: const Color.fromARGB(255, 149, 167, 164),
-          padding: const EdgeInsets.symmetric(
-            vertical: 12.0,
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color(0xFF006A5B),
+            padding: const EdgeInsets.symmetric(
+              vertical: 12.0,
+              horizontal: 115.0,
+            ),
+            foregroundColor: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(25),
+            ),
           ),
-          foregroundColor: Colors.white,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
-          ),
-        ),
-        onPressed: () {},
-        child: const CircularProgressIndicator());
+          onPressed: () {},
+          child: const CircularProgressIndicator(
+            color: Color.fromARGB(255, 149, 167, 164),
+          )),
+    );
   }
 }
